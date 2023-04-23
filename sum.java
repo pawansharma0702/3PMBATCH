04193de59e401f8e0b6d0b2c22b5ca13ebc94ee8
pawan.sharma0702 <pawan.sharma0702@gmail.com>
2023-04-23 15:33:30 +0530

    My sum File

---
+++ sum.java
@@ -0,0 +1,19 @@
+public class git{
+
+public static void main (String [] args)
+{
+	int a=10;
+	int b=20;
+	
+	int c= a+b;
+	int d=a-b
+	
+	System.out.println("The sum is: "+ c);
+	System.out.println("The sum is: "+ d);
+	System.out.println("The sum is: "+ d);
+	// New line added
+		System.out.println("The sum is: "+ d);
+}
+
+
+}
