fe3c9a79a147d2d792171864ccdc7f4dcd7a1596
pawan.sharma0702 <pawan.sharma0702@gmail.com>
2023-04-23 15:41:28 +0530

    Sub file commited

---
+++ sub.java
@@ -0,0 +1,16 @@
+public class gitt{
+
+public static void main (String [] args)
+{
+	int a=10;
+	int b=20;
+	
+	int c= a+b;
+	int d=a-b
+	
+	System.out.println("The sum is: "+ c);
+	System.out.println("The sum is: "+ d);
+}
+
+
+}
